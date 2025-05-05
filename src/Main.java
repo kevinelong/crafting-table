ad214fc43e94a824b25b0748046061bcd336642c
Kevin Ernest Long <kevinelong@gmail.com>
2025-05-05 16:01:18 -0700

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    static HashMap<String,String> recipes = new HashMap<>();
+    public static void main(String[] args) {
+        recipes.put("icecream-chocolate-cherry", "SUNDAY");
+        recipes.put("icecream-banana-pineapple", "BANANA-SPLIT");
+
+        List<String> items = List.of("icecream", "chocolate", "cherry");
+        System.out.println(craftThing(items));
+
+        List<String> items2 = List.of("ice", "chocolate", "cherry");
+        System.out.println(craftThing(items2));
+
     }
+
+    static String craftThing(List<String> list) {
+        String mashed = String.join("-", list);
+        System.out.println(mashed);
+        if(recipes.containsKey(mashed)){
+            return recipes.get(mashed);
+        }
+        return "NOPE";
+    }
+
+//    static makeSunday(String i, String ii, String iii) {
+//
+//    }
 }
